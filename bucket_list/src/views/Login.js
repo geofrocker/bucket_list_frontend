beea829c22c70c50586295e419a29852c5801035
ridgekimani <ridgekimani@gmail.com>
2017-08-21 23:13:24 +0300

    Update login functionality to consume API. #TODO: Redirect after login

--- bucket_list/src/views/Login.js
+++ bucket_list/src/views/Login.js
@@ -1,7 +1,43 @@
 import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
+import {Link, Redirect } from 'react-router-dom';
+import axios from 'axios';
 
 class Login extends Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {email: '', password: '', redirect: false};
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    }
+    handleEmailChange(event){
+        this.setState({email: event.target.value})
+    }
+
+    handlePasswordChange(event) {
+        this.setState({password: event.target.value})
+    }
+
+    handleSubmit(event){
+        axios({
+            url : 'http://ridge-bucket-list-api.herokuapp.com/api/v1/auth/login',
+            data: {email: this.state.email, password: this.state.password},
+            datatype: "json",
+            method: "post"
+        })
+            .then(function (response) {
+                this.setState({redirect: true})
+                window.location.href = '/'
+        })
+            .catch(function (xhr) {
+                if ("response" in xhr){
+                    alert(xhr.response.data.error);
+                }
+            });
+        event.preventDefault()
+    }
+
     render(){
         return(
             <div className="auth">
@@ -14,14 +50,14 @@ class Login extends Component{
                         </header>
                         <div className="auth-content">
                             <p className="text-xs-center">LOGIN TO CONTINUE</p>
-                            <form id="login-form" method="POST" onsubmit="return false">
+                            <form id="login-form" method="POST" onSubmit={this.handleSubmit}>
                                 <div className="form-group">
-                                    <label for="email">Email Address</label>
-                                    <input type="text" className="form-control underlined" name="email" id="email" placeholder="Your Email address" />
+                                    <label>Email Address</label>
+                                    <input type="text" className="form-control underlined"  value={this.state.email} onChange={this.handleEmailChange} name="email" id="email" placeholder="Your Email address" />
                                 </div>
                                 <div className="form-group">
-                                    <label for="password">Password</label>
-                                    <input type="password" className="form-control underlined" name="password" id="password" placeholder="Your Password" />
+                                    <label>Password</label>
+                                    <input type="password" className="form-control underlined" value={this.state.password} onChange={this.handlePasswordChange} name="password" id="password" placeholder="Your Password" />
                                 </div>
 
                                 <div className="form-group">
