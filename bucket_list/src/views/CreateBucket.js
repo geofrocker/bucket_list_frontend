899818ec24355b7eec688c12bc54b29c8f71d42e
ridgekimani <ridgekimani@gmail.com>
2017-08-27 13:32:06 +0300

    Working create bucket

--- bucket_list/src/views/CreateBucket.js
+++ bucket_list/src/views/CreateBucket.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 import Header from '../components/Header';
 import Sidebar from '../components/Sidebar';
+import {Redirect} from 'react-router-dom';
 import axios from 'axios'
 
 class CreateBucket extends Component{
     constructor(props){
         super(props);
-        this.state = {bucket_name:'', description:'', category:'', isAuthorized:false};
+        this.state = {bucket_name:'', description:'', category:'', token: '',isAuthorized:false};
         this.handleDescriptionChange = this.handleDescriptionChange.bind(this);
         this.handleNameChange = this.handleNameChange.bind(this);
         this.handleCategoryChange = this.handleCategoryChange.bind(this);
@@ -15,16 +16,20 @@ class CreateBucket extends Component{
 
     componentDidMount() {
         axios({
-            url: 'https://ridge-bucket-list-api.herokuapp.com/api/v1/bucketlists/',
+            // url: 'https://ridge-bucket-list-api.herokuapp.com/api/v1/bucketlists/',
+            url: 'http://127.0.0.1:5000/api/v1/bucketlists/',
             method: "GET",
-            headers: {token: "This is nice"}
-        }).then(function (response) {
-            alert(response.response.data)
+            headers: {
+                'token': window.localStorage.getItem('token'),
+                'Content-Type': 'application/json'
+            }
         })
-            .catch(function (xhr) {
-                alert(xhr.response.data.error)
+            .then((response)=>{
+                this.setState({isAuthorized:true});
+        })
+            .catch((xhr) =>{
+                alert(xhr);
             });
-
     }
 
     handleNameChange(event){
@@ -39,7 +44,9 @@ class CreateBucket extends Component{
         this.setState({category: event.target.value})
     }
 
+
     handleSubmit(event){
+        event.preventDefault();
         let data = {
             bucket_name:this.state.bucket_name,
             description: this.state.description,
@@ -47,23 +54,41 @@ class CreateBucket extends Component{
         };
         
         axios({
-            url: 'http://ridge-bucket-list-api.herokuapp.com/api/v1/bucketlists/',
+            // url: 'http://ridge-bucket-list-api.herokuapp.com/api/v1/bucketlists/',
+            url: 'http://127.0.0.1:5000/api/v1/bucketlists/',
             method: 'POST',
             datatype: "json",
             data: data,
+            headers: {
+                'token': window.localStorage.getItem('token'),
+                'Content-Type': 'application/json'
+            }
             
         })
-            .then(function (response) {
-                alert(response.response.data)
+            .then((response)=>{
+                this.setState({redirect:true});
         })
-            . catch(function (xhr) {
-                    if ("response" in xhr){
-                        alert(xhr.response.data.error);
-            }
-        })
+            .catch((xhr) =>{
+                console.log(JSON.stringify(xhr));
+                alert(xhr.response.data.error);
+        });
     }
 
     render(){
+        if (this.state.isAuthorized === false){
+            return(
+                <div>
+                    <article className="content item-editor-page">
+                        <div className="card card-block">
+                            <p>Unauthorized! Please log in</p>
+                        </div>
+                    </article>
+                </div>
+            )
+        }
+        else if (this.state.redirect === true){
+            return <Redirect to="/bucketlists/view"/>
+        }
         return(
             <div>
                 <Header/>
@@ -75,7 +100,7 @@ class CreateBucket extends Component{
                         </h3>
                     </div>
 
-                    <form name="item" method="post" onsubmit={this.handleSubmit}>
+                    <form method="post" onSubmit={this.handleSubmit}>
                         <div className="card card-block">
                             <div className="form-group row">
                                 <label className="col-sm-2 form-control-label text-xs-right">
@@ -110,9 +135,6 @@ class CreateBucket extends Component{
                             <div className="col-sm-10 col-sm-offset-2">
                                 <button type="submit" className="btn btn-primary">
                                     Add
-                                </button>
-                                <button type="submit" className="btn btn-primary">
-                                    Add & New
                                 </button>
                             </div>
                         </div>
