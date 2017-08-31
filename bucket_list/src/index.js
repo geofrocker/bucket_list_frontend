f540149fb99978d6df71c5ea340826dba8de8f2f
ridgekimani <ridgekimani@gmail.com>
2017-08-31 19:18:15 +0300

    Add more paths

--- bucket_list/src/index.js
+++ bucket_list/src/index.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {render} from 'react-dom';
 import CreateBucket from './views/CreateBucket';
 import ViewBuckets from './views/ViewBuckets';
+import ViewBucket from './views/ViewBucket'
 import UpdateBucket from './views/UpdateBucket'
 import Register from './views/Register';
 import Login from './views/Login';
@@ -17,7 +18,8 @@ class Index extends Component{
                     <Route path="/login/" component={Login}/>
                     <Route path="/bucketlists/create/" component={CreateBucket}/>
                     <Route path="/bucketlists/view/" component={ViewBuckets}/>
-                    <Route path="/bucketlists/update/" component={UpdateBucket} />
+                    <Route path="/bucketlists/views/:bucket_id" component={ViewBucket}/>
+                    <Route path="/bucketlists/update/:bucket_id" component={UpdateBucket} />
                 </Switch>
             </div>
         )
